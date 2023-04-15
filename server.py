2dcd5bf9d55370475f12961e383f934fe8d52b08
Ubuntu <ubuntu@ip-172-31-41-24.ap-northeast-2.compute.internal>
2023-04-15 04:23:07 +0000

    add server.py

---
+++ server.py
@@ -0,0 +1,57 @@
+import socket
+import time
+import struct
+import os
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Enable MPTCP
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setsockopt(socket.IPPROTO_TCP, 42, 1) # 42 is the MPTCP_ENABLED option
+
+# Bind the socket object to a specific address and port
+server_socket.bind(('172.31.41.24', 8888))
+
+# Listen for incoming connections
+server_socket.listen(1)
+
+print('Waiting for incoming connections...')
+
+# Accept incoming connections
+client_socket, client_address = server_socket.accept()
+
+print('Accepted connection from:', client_address)
+
+# Receive the video frames from the server
+while True:
+    # Receive the message header from the server
+    header_data = b''
+    while len(header_data) < 16:
+        chunk = client_socket.recv(16 - len(header_data))
+        if not chunk:
+            break
+        header_data += chunk
+    if len(header_data) < 16:
+        break
+    
+    print('Header', len(header_data))
+    # Unpack the timestamp and frame size fields from the message header
+    timestamp, frame_size = struct.unpack('dL', header_data)
+    
+    # Receive the frame data from the server
+    frame_data = b''
+    while len(frame_data) < frame_size:
+        chunk = client_socket.recv(frame_size - len(frame_data))
+        if not chunk:
+            break
+        frame_data += chunk
+    if len(frame_data) < frame_size:
+        break
+    
+    # Print the timestamp and frame size
+    print('Received frame with timestamp', timestamp, 'and size', frame_size)
+
+# Close the connection
+client_socket.close()
+server_socket.close()
