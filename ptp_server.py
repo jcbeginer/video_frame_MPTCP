843b3bbcefd57479bbb11002c4a506e2617edf94
woosung <46365676+jcbeginer@users.noreply.github.com>
2023-05-12 12:09:50 +0900

    Add files via upload
    this is for time sync between client and server

---
+++ ptp_server.py
@@ -0,0 +1,27 @@
+import socket
+import time
+
+SERVER_IP = '13.125.241.159'  # Replace with the server's IP address
+PORT = 9090
+
+def synchronize_time():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    send_time = time.time()
+    client_socket.sendto('SYNC_REQUEST'.encode(), (SERVER_IP, PORT))
+
+    server_timestamp = client_socket.recv(1024).decode()
+    receive_time = time.time()
+
+    round_trip_time = receive_time - send_time
+    adjusted_server_time = float(server_timestamp) + (round_trip_time / 2)
+
+    return adjusted_server_time
+ 
+def main(): 
+    print('Start client') 
+    adjusted_server_time = synchronize_time() 
+    print(f'Adjusted server time: {adjusted_server_time}') 
+ 
+if __name__ == "__main__": 
+    main()
