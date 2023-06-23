0c1e6b7cff5d7f2d746b65fa383d558b985b4670
woosung <46365676+jcbeginer@users.noreply.github.com>
2023-06-23 18:46:18 +0900

    Update video_analytics_server_trashbit.py
    I change server_Trashbit code
    time.sleep 60ms  -> just add 60 ms when client get final packet!
    (to handle multi-threading related issues - like memory management and sequential processing etc)

--- video_analytics/video_analytics_server_trashbit.py
+++ video_analytics/video_analytics_server_trashbit.py
@@ -4,12 +4,12 @@ import struct
 import os
 import threading
 from datetime import datetime
-import queue
+
 
 
 # Handle client connection
 def receive_frame():
-    
+    while True:
       header_data = b''
       while len(header_data) < 16:
           chunk = client_socket.recv(16 - len(header_data))
@@ -21,8 +21,6 @@ def receive_frame():
 
       print('Header', len(header_data))
       timestamp, frame_size, idx = struct.unpack('tfi', header_data)
-      received_timestamp = float(time.time())
-      e2e_delay = received_timestamp-timestamp
         
       frame_data = b''
       while len(frame_data) < frame_size:
@@ -32,30 +30,22 @@ def receive_frame():
           frame_data += chunk
       if len(frame_data) < frame_size:
           break
-      # Pack the frame data and header into a message
+    #received time stamp
+      received_timestamp = float(time.time())
+      e2e_delay = received_timestamp-timestamp
+    # Pack the frame data and header into a message
       timestamp_packed = struct.pack('t', timestamp)
       frame_size_packed = struct.pack('f', frame_size)
       idx_packed = struct.pack('i', idx)
       message = timestamp_packed + frame_size_packed + idx_packed  + frame_data
                 
-      print('Received frame with timestamp:', timestamp,'E2E delay:',e2e_delay, 'and size:', frame_size)
+      print('idx :', idx,'E2E delay:',e2e_delay, 'and size:', frame_size)
 
-      time.sleep(0.06)
+
       client_socket.sendall(message)
 
       with open(filename, 'a') as f:
           f.write('sender's timestamp ,{},received timestamp ,{}, E2E delay ,{}, and size ,{},\n'.format(timestamp,received_timestamp,e2e_delay, frame_size))
-
-
-# Close the server socket
-# This will never be reached in the current script, as the while True loop does not terminate
-# It would need to be called in a shutdown routine
-
-def terminate(client_socket, server_socket):
-    client_socket.close()
-    server_socket.close()
-    with open(filename, 'a') as f:
-        f.write('END ----------------------------------------------------------------------------\n')
 
 
 # Create a socket object
@@ -72,9 +62,6 @@ server_socket.bind(('172.31.41.24', 8888))
 server_socket.listen(1)
 
 print('Waiting for incoming connections...')
-
-# set timer for termination
-# timer = float(time.time())
 
 # Accept incoming connections
 client_socket, client_address = server_socket.accept()
@@ -95,14 +82,10 @@ filename = './logging/video_analytics_server_log{}.txt'.format(date_string)
 with open(filename, 'a') as f:
     f.write('start--------------------------------------------\n')
 
-threads =[]
-# Start a new thread to handle this client
-while True:
-    thread = threading.Thread(target=process_frames)
-    threads.append(thread)
-    thread.start()
-  
-for thread in threads:
-  thread.join()
+receive_frame()
 
-terminate(client_socket, server_socket)
+client_socket.close()
+server_socket.close()
+with open(filename, 'a') as f:
+    f.write('END ----------------------------------------------------------------------------\n')
+
