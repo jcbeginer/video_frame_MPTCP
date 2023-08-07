c9d07359037efbd8a388bc0a5c7f652bbc9d1899
woosung <46365676+jcbeginer@users.noreply.github.com>
2023-08-07 13:16:31 +0900

    Update video_analytics_server_trashbit_2.py

--- video_analytics/video_analytics_server_trashbit_2.py
+++ video_analytics/video_analytics_server_trashbit_2.py
@@ -40,13 +40,14 @@ def receive_frame():
       idx_packed = struct.pack('i', idx)
        
     #frame_size of sending is 8192 (1KB)
-      message = timestamp_packed + frame_size_packed + idx_packed
-      frame_data[:len(message)] = message
+      message = timestamp_packed + frame_size_packed + idx_packed+frame_data
+      #frame_data[:len(message)] = message
                 
       print('idx :', idx,'E2E delay:',e2e_delay, 'and size:', frame_size)
 
       #client_socket.sendall(idx_packed)
-      client_socket.sendall(frame_data[:8192+20])
+      #client_socket.sendall(frame_data[:8192+20])
+      client_socket.sendall(message)
 
       with open(filename, 'a') as f:
           f.write("sender's timestamp ,{},received timestamp ,{}, E2E delay ,{}, and size ,{},\n".format(timestamp,received_timestamp,e2e_delay, frame_size))
