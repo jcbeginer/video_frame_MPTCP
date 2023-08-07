e0133a0b395e38051517798e66c44203229ca920
woosung <46365676+jcbeginer@users.noreply.github.com>
2023-08-07 15:01:02 +0900

    Update video_analytics_client_trashbit_2.py

--- video_analytics/video_analytics_client_trashbit_2.py
+++ video_analytics/video_analytics_client_trashbit_2.py
@@ -89,7 +89,7 @@ def receive_frames(client_socket, frame_size):
       #print('Header', len(header_data))
       sent_timestamp, received_frame_size, idx = struct.unpack('dLi', header_data)
 
-      frame_size -=20
+      #frame_size -=20 애초에 저쪽에서 작게들어와서 괜찮음
       frame_data = b''
       while len(frame_data) < frame_size:
           chunk = client_socket.recv(frame_size - len(frame_data))
