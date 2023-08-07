a02a5522978ac9f56d99162338367d30be777c68
woosung <46365676+jcbeginer@users.noreply.github.com>
2023-08-07 12:15:41 +0900

    Update video_analytics_server_trashbit_2.py

--- video_analytics/video_analytics_server_trashbit_2.py
+++ video_analytics/video_analytics_server_trashbit_2.py
@@ -6,7 +6,7 @@ import threading
 from datetime import datetime
 
 
-frame_data = bytearr(b'0'*8192)
+frame_data = bytearray(b'0'*8192)
 # Handle client connection
 def receive_frame():
     while True:
