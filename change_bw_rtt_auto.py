f0c8b87b74f4b408b4f9b3ac159c8abbfc7f400d
woosung <46365676+jcbeginer@users.noreply.github.com>
2023-10-18 18:40:52 +0900

    Update change_bw_rtt_auto.py

--- change_bw_rtt_auto.py
+++ change_bw_rtt_auto.py
@@ -14,13 +14,14 @@ def change_network_conditions_from_csv(file):
             latency_WiFi0 = row['delay_WiFi0']
             cmd_WiFi0 = f'sudo tc qdisc replace dev WiFi0 root netem rate {bandwidth_WiFi0}mbit delay {latency_WiFi0}ms limit 1800'
             print(cmd_WiFi0)
-            subprocess.run(cmd_WiFi0, shell=True, check=True)
-
+            
             # WiFi1에 대한 bandwidth와 latency 값을 추출 및 설정
             bandwidth_WiFi1 = row['bandwidth_WiFi1']
             latency_WiFi1 = row['delay_WiFi1']
             cmd_WiFi1 = f'sudo tc qdisc replace dev WiFi1 root netem rate {bandwidth_WiFi1}mbit delay {latency_WiFi1}ms limit 1800'
             print(cmd_WiFi1)
+            
+            subprocess.run(cmd_WiFi0, shell=True, check=True)
             subprocess.run(cmd_WiFi1, shell=True, check=True)
 
             # 다음 반복 전에 100ms 대기
